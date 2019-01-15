e6053c769f2e9c2c6213c768632938ceae1d4ae5
kangaro0 <johannes_herbert@yahoo.de>
2019-01-15 17:41:58 +0100

    decided for websocket implementation

--- signaling/dist/enums.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var MessageType;
-(function (MessageType) {
-    MessageType[MessageType["CONNECT"] = 0] = "CONNECT";
-    MessageType[MessageType["MESSAGE"] = 1] = "MESSAGE";
-    MessageType[MessageType["DISCONNECT"] = 2] = "DISCONNECT";
-    MessageType[MessageType["ERROR"] = 3] = "ERROR";
-})(MessageType = exports.MessageType || (exports.MessageType = {}));
--- signaling/src/enums.ts
+++
@@ -1,6 +0,0 @@
-export enum MessageType {
-    CONNECT,
-    MESSAGE,
-    DISCONNECT,
-    ERROR
-}
